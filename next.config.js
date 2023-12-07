8f9ed430665ff57feb12625e1e16b7b6f91141d4
eltonleao <3lton.leao@gmail.com>
2023-12-07 20:24:28 -0300

    Commented out isProd variable and basePath option in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === "production";
+// const isProd = process.env.NODE_ENV === "production";
 
 const nextConfig = {
   output: "export",
-  basePath: isProd ? "/desafio-meztra-layout/" : "",
   images: {
     remotePatterns: [
       {
