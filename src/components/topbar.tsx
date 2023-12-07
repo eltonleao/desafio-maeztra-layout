8607f333647e11e04ccd9309252e4d0eac41dc69
eltonleao <3lton.leao@gmail.com>
2023-12-07 19:55:14 -0300

    Fix typo in topbar component

--- src/components/topbar.tsx
+++ src/components/topbar.tsx
@@ -4,9 +4,7 @@ export default function TopBar() {
   return (
     <>
       <div className={`${styles.topbar} fixed top-0 z-50 border-0`}>
-        <span>
-          Acompanhe as melhores promoções disponíveis aqui na Maeztra.
-        </span>
+        <span>Acompanhe as melhores promoções disponíveis aqui na Maeztra</span>
       </div>
     </>
   );
