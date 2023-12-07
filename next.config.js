d3036899cb8f455da83ffaf4894a746d4f1682e8
eltonleao <3lton.leao@gmail.com>
2023-12-07 20:05:49 -0300

    Update next.config.js for production deployment

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
   output: "export",
-  assetPrefix: "/desafio-meztra-layout/",
+  basePath: isProd ? "/desafio-meztra-layout/" : "",
   images: {
     remotePatterns: [
       {
