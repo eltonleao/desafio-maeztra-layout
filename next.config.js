0f4cb9e4603b8e2c485e9a67d24d19bc4dab110d
eltonleao <3lton.leao@gmail.com>
2023-12-07 19:35:58 -0300

    Remove commented code in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,14 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
-  // images: {
-  //   remotePatterns: [
-  //     {
-  //       protocol: "https",
-  //       hostname: "**",
-  //     },
-  //   ],
-  // },
+  /* config options here */
 };
 
 module.exports = nextConfig;
